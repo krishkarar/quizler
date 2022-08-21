463ceb3da2fd6d9eacc4ba70dc2c646ca2362b0c
Kushagra Karar <krishkararajay@gmail.com>
2022-08-21 23:13:51 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/quizbaji/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.quizbaji
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,328 @@
+import 'package:flutter/material.dart';
+//TODO: Step 2 - Import the rFlutter_Alert package here.
+import 'package:rflutter_alert/rflutter_alert.dart';
+import 'quizBank.dart';
+
+QuestionBank questionBnak = QuestionBank();
+
+void main() => runApp(Quizzler());
+
+class Quizzler extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizPage(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class QuizPage extends StatefulWidget {
+  @override
+  _QuizPageState createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeeper = [];
+  int score  = 0;
+
+  void checkAnswer(bool userPickedAnswer) {
+    bool correctAnswer = questionBnak.getCorrectAns();
+
+    setState(() {
+
+      if(questionBnak.isFinished()){
+        Alert(
+          context: context,
+          type: AlertType.warning,
+          title: "QUIZ OVER ALERT",
+          desc: "You have attempted all the questions with a score of $score+1, click cancel to restart",
+          buttons: [
+
+            DialogButton(
+              child: Text(
+                "CANCEL",
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              ),
+              onPressed: () => Navigator.pop(context),
+              gradient: LinearGradient(colors: [
+                Color.fromRGBO(116, 116, 191, 1.0),
+                Color.fromRGBO(52, 138, 199, 1.0)
+              ]),
+            )
+          ],
+        ).show();
+
+        questionBnak.reset();
+        scoreKeeper.clear();
+        score = 0;
+
+      }
+      //TODO: Step 4 - Use IF/ELSE to check if we've reached the end of the quiz. If true, execute Part A, B, C, D.
+      //TODO: Step 4 Part A - show an alert using rFlutter_alert (remember to read the docs for the package!)
+      //HINT! Step 4 Part B is in the quiz_brain.dart
+      //TODO: Step 4 Part C - reset the questionNumber,
+      //TODO: Step 4 Part D - empty out the scoreKeeper.
+
+      //TODO: Step 5 - If we've not reached the end, ELSE do the answer checking steps below 👇
+      else{
+        if (userPickedAnswer == correctAnswer) {
+          scoreKeeper.add(Icon(
+            Icons.check,
+            color: Colors.green,
+          ));
+          score++;
+        } else {
+          scoreKeeper.add(Icon(
+            Icons.close,
+            color: Colors.red,
+          ));
+        }
+        questionBnak.nextQuestion();
+      }
+
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                questionBnak.getQuestionText(),
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: TextButton(
+              style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.green)),
+              child: Text(
+                'True',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+              onPressed: () {
+                //The user picked true.
+                checkAnswer(true);
+              },
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: TextButton(
+              style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.green)),
+              child: Text(
+                'False',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ),
+              onPressed: () {
+                //The user picked false.
+                checkAnswer(false);
+              },
+            ),
+          ),
+        ),
+        Row(
+          children: scoreKeeper,
+        )
+      ],
+    );
+  }
+}
+
+/*
+question1: 'You can lead a cow down stairs but not up stairs.', false,
+question2: 'Approximately one quarter of human bones are in the feet.', true,
+question3: 'A slug\'s blood is green.', true,
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import 'package:flutter/material.dart';
+// import 'package:quizbaji/quizBank.dart';
+//
+// QuestionBank questionBank = QuestionBank();
+//
+// void main() => runApp(Quizzler());
+//
+// class Quizzler extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         backgroundColor: Colors.grey.shade900,
+//         body: SafeArea(
+//           child: Padding(
+//             padding: EdgeInsets.symmetric(horizontal: 10.0),
+//             child: QuizPage(),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+//
+// class QuizPage extends StatefulWidget {
+//   @override
+//   _QuizPageState createState() => _QuizPageState();
+// }
+//
+// class _QuizPageState extends State<QuizPage> {
+//   @override
+//   List <Icon> scoreKeeper = [];
+//   // List <String> questions = [
+//   //   'You can lead a cow down stairs but not up stairs.',
+//   //   'Approximately one quarter of human bones are in the feet.',
+//   //   'A slug\'s blood is green.',
+//   // ];
+//   // List <bool> answers = [
+//   //   false,
+//   //   true,
+//   //   true,
+//   //
+//   // ];
+//   // int questionNumber = 0;
+//
+//   // Question q1 = Question('You can lead a cow down stairs but not up stairs.', false);
+//
+//   // List <Question> QnA = [
+//   //   Question('You can lead a cow down stairs but not up stairs.', false),
+//   //   Question('Approximately one quarter of human bones are in the feet.', true),
+//   //   Question('A slug\'s blood is green.', true),
+//   //
+//   // ];
+//   Widget build(BuildContext context) {
+//     return Column(
+//       mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//       crossAxisAlignment: CrossAxisAlignment.stretch,
+//       children: <Widget>[
+//         Expanded(
+//           flex: 5,
+//           child: Padding(
+//             padding: EdgeInsets.all(10.0),
+//             child: Center(
+//               child: Text(
+//                 questionBank.getQuestionText(),
+//                 textAlign: TextAlign.center,
+//                 style: TextStyle(
+//                   fontSize: 25.0,
+//                   color: Colors.white,
+//                 ),
+//               ),
+//             ),
+//           ),
+//         ),
+//         Expanded(
+//           child: Padding(
+//             padding: EdgeInsets.all(15.0),
+//             child: TextButton(
+//               style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.green)),
+//               child: Text(
+//                 'True',
+//                 style: TextStyle(
+//                   color: Colors.white,
+//                   fontSize: 20.0,
+//                 ),
+//               ),
+//               onPressed: () {
+//                 //The user picked true.
+//                 bool correctAns = questionBank.getCorrectAns();
+//                 if(correctAns == true){
+//                   print("correct answer");
+//                 }
+//                 else{
+//                   print("wrong answer");
+//                 }
+//
+//                 setState((){
+//                   questionBank.nextQuestion();
+//                 });
+//
+//
+//               },
+//             ),
+//           ),
+//         ),
+//         Expanded(
+//           child: Padding(
+//             padding: EdgeInsets.all(15.0),
+//             child: TextButton(
+//               style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.red)),
+//               child: Text(
+//                 'False',
+//                 style: TextStyle(
+//                   fontSize: 20.0,
+//                   color: Colors.white,
+//                 ),
+//               ),
+//               onPressed: () {
+//                 //The user picked false.
+//                 bool correctAns = questionBank.getCorrectAns();
+//
+//                 if(correctAns == false){
+//                   print("correct answer");
+//                 }
+//                 else{
+//                   print("wrong answer");
+//                 }
+//                 setState((){
+//                   questionBank.nextQuestion();
+//                 });
+//               },
+//             ),
+//           ),
+//         ),
+//         //TODO: Add a Row here as your score keeper
+//         Row(
+//           children: scoreKeeper,
+//         )
+//       ],
+//     );
+//   }
+// }
+//
+// /*
+// question1: 'You can lead a cow down stairs but not up stairs.', false,
+// question2: 'Approximately one quarter of human bones are in the feet.', true,
+// question3: 'A slug\'s blood is green.', true,
+// */
